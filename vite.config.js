67c579ad69330dba8cf65406c181544f9d3ae78e
Sachin Dhayatadak <sachindhayatadake@sdlccorp.com>
2025-03-25 10:30:13 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Ensures accessibility from external requests
+    port: parseInt(process.env.PORT) || 5173  // Uses Render's assigned port
+  },
+  preview: {
+    host: true,  
+    port: parseInt(process.env.PORT) || 4173  // Uses Render's assigned port
+  }
 })
