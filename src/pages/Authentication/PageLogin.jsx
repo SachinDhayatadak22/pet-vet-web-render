a9656fc36b634a105e79dc9a6f43646e5fa3cb9f
Sachin Dhayatadak <sachindhayatadake@sdlccorp.com>
2025-03-25 10:48:35 +0530

    Change in login

--- src/pages/Authentication/PageLogin.jsx
+++ src/pages/Authentication/PageLogin.jsx
@@ -10,10 +10,10 @@ import { toast } from 'react-toastify';
 import AuthBaseLayout from '../../layout/AuthBaseLayout';
 import adminIcon from '../../images/authentication/adminIcon.svg'
 
-const PageLogin = ({mode}) => {
+const PageLogin = ({ mode }) => {
     const [role, setRole] = useState(mode);
     const navigate = useNavigate();
-    const dispatch = useDispatch(); 
+    const dispatch = useDispatch();
     const [loading, setLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
     const [showPassword, setShowPassword] = useState(true);
@@ -79,65 +79,63 @@ const PageLogin = ({mode}) => {
                     </h1>
                     <p className="text-[16px] font-medium mb-[29px] text-textgray tracking-[0.32px]">We are happy to see you again, Enter
                         your Email and Password</p>
-                    {mode!=="admin"?(
-                      <>
-                          <div className="relative flex items-center justify-center w-full pb-[45px] pt-[35px]">
-                              <button
-                                className={`absolute left-[15px] w-1/2 py-[10px] text-center text-[14px] font-medium rounded-full transition-all duration-300 ${
-                                  role === "vet"
-                                    ? "bg-secondary text-white z-10"
-                                    : "text-textTertiary z-0 bg-primary"
-                                }`}
-                                onClick={() => setRole("vet")}
-                              >
-                                  Veterinarian
-                              </button>
-                              <button
-                                className={`absolute right-[15px] w-1/2 py-[10px] text-center text-[14px] font-medium rounded-full transition-all duration-300 ${
-                                  role === "nurse"
-                                    ? "bg-secondary text-white z-10"
-                                    : "text-textTertiary z-0 bg-primary"
-                                }`}
-                                onClick={() => setRole("nurse")}
-                              >
-                                  Nurse
-                              </button>
-                          </div>
-                      </>
-                    ):(
-                      <div className="flex items-center justify-center w-full mb-[45px]">
-                          <img src={adminIcon} alt="icon" className="mr-3"/>
-                          <span className="uppercase text-[18px] font-semibold text-textcolor">Administrator</span>
-                      </div>
+                    {mode !== "admin" ? (
+                        <>
+                            <div className="relative flex items-center justify-center w-full pb-[45px] pt-[35px]">
+                                <button
+                                    className={`absolute left-[15px] w-1/2 py-[10px] text-center text-[14px] font-medium rounded-full transition-all duration-300 ${role === "vet"
+                                        ? "bg-secondary text-white z-10"
+                                        : "text-textTertiary z-0 bg-primary"
+                                        }`}
+                                    onClick={() => setRole("vet")}
+                                >
+                                    Veterinarian
+                                </button>
+                                <button
+                                    className={`absolute right-[15px] w-1/2 py-[10px] text-center text-[14px] font-medium rounded-full transition-all duration-300 ${role === "nurse"
+                                        ? "bg-secondary text-white z-10"
+                                        : "text-textTertiary z-0 bg-primary"
+                                        }`}
+                                    onClick={() => setRole("nurse")}
+                                >
+                                    Nurse
+                                </button>
+                            </div>
+                        </>
+                    ) : (
+                        <div className="flex items-center justify-center w-full mb-[45px]">
+                            <img src={adminIcon} alt="icon" className="mr-3" />
+                            <span className="uppercase text-[18px] font-semibold text-textcolor">Administrator</span>
+                        </div>
                     )}
                 </div>
                 <form onSubmit={handleSubmit} className="w-full">
                     <div>
                         <label className="sr-only">Email</label>
                         <input
-                          type="email"
-                          name="email"
-                          placeholder="Email"
-                          value={userDetails.email}
-                          onChange={handleChange}
-                          className={`w-full px-[16px] py-[10px] text-[14px] font-medium mb-[18px] rounded-lg bg-primary text-textcolor outline-none focus:ring-2 focus:ring-secondary ${errorState.email ? 'border border-red-700' : ''
-                          }`}
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            value={userDetails.email}
+                            onChange={handleChange}
+                            className={`w-full px-[16px] py-[10px] text-[14px] font-medium mb-[18px] rounded-lg bg-primary text-textcolor outline-none focus:ring-2 focus:ring-secondary ${errorState.email ? 'border border-red-700' : ''
+                                }`}
                         />
                     </div>
                     <div className="relative">
                         <input
-                          type={showPassword ? 'password' : 'text'}
-                          name="password"
-                          placeholder="Password"
-                          value={userDetails.password}
-                          onChange={handleChange}
-                          className="w-full px-[16px] py-[10px] text-[14px] font-medium mb-[18px] rounded-lg bg-primary text-textcolor outline-none focus:ring-2 focus:ring-secondary"
+                            type={showPassword ? 'password' : 'text'}
+                            name="password"
+                            placeholder="Password"
+                            value={userDetails.password}
+                            onChange={handleChange}
+                            className="w-full px-[16px] py-[10px] text-[14px] font-medium mb-[18px] rounded-lg bg-primary text-textcolor outline-none focus:ring-2 focus:ring-secondary"
                         />
                         <button
-                          type="button"
-                          onClick={() => setShowPassword((prev) => !prev)}
-                          className="absolute right-3 top-3 text-gray-400"
-                          aria-label="Toggle password visibility"
+                            type="button"
+                            onClick={() => setShowPassword((prev) => !prev)}
+                            className="absolute right-3 top-3 text-gray-400"
+                            aria-label="Toggle password visibility"
                         >
                             {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
                         </button>
@@ -145,31 +143,34 @@ const PageLogin = ({mode}) => {
                     <div className="flex items-center justify-between text-gray-400 mb-[32px]">
                         <label className="flex items-center space-x-2">
                             <input
-                              type="checkbox"
-                              checked={isChecked}
-                              onChange={() => setIsChecked(!isChecked)}
-                              className="text-secondary ring-secondary"
+                                type="checkbox"
+                                checked={isChecked}
+                                onChange={() => setIsChecked(!isChecked)}
+                                className="text-secondary ring-secondary"
                             />
                             <span className="text-xs text-textgray font-medium">Remember for 30 days</span>
                         </label>
                         <span onClick={() => navigate('/forget-password')} className="text-secondary font-medium text-xs cursor-pointer">
-                        Forget Password?
-                    </span>
+                            Forget Password?
+                        </span>
                     </div>
                     <button
-                      type="submit"
-                      className="w-full py-[10px] rounded-3xl bg-secondary text-white text-[16px] font-semibold "
-                      disabled={loading}
+                        type="submit"
+                        className="w-full py-[10px] rounded-3xl bg-secondary text-white text-[16px] font-semibold "
+                        disabled={loading}
                     >
                         {loading ? 'Signing in...' : 'Sign In'}
                     </button>
                     {errorMessage && <p className="text-red-500 text-center mt-4">{errorMessage}</p>}
                 </form>
-                {/* <div className="mt-[31px]">
-                    <span className="text-[14px] font-medium text-textgray">
-                        Don't have an account ? <a href="#" className="text-secondary" onClick={(e) => {e.preventDefault();navigate("/signup")}}>Create an account</a>
-                    </span>
-                </div> */}
+                <div className="mt-[31px]">
+                    {
+                        userDetails?.role === "admin" &&
+                        <span className="text-[14px] font-medium text-textgray">
+                            Don't have an account ? <a href="#" className="text-secondary" onClick={(e) => { e.preventDefault(); navigate("/signup") }}>Create an account</a>
+                        </span>
+                    }
+                </div>
             </div>
         </AuthBaseLayout>
     );
