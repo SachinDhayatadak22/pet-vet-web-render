9d8728f3517fcdd17dd98b1094eebfde06f377ee
Sachin Dhayatadak <sachindhayatadake@sdlccorp.com>
2025-03-25 10:37:27 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,  // Ensures accessibility from external requests
-    port: parseInt(process.env.PORT) || 5173  // Uses Render's assigned port
+    host: true,
+    port: parseInt(process.env.PORT) || 5173,
+    strictPort: true,
+    allowedHosts: ['pet-vet-web-render-1.onrender.com']
   },
   preview: {
-    host: true,  
-    port: parseInt(process.env.PORT) || 4173  // Uses Render's assigned port
+    host: true,
+    port: parseInt(process.env.PORT) || 4173,
+    strictPort: true,
+    allowedHosts: ['pet-vet-web-render-1.onrender.com']
   }
 })
